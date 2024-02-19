13ae94bff40b4e06dbce0bbec12c429b8afe8088
TheBharatMishra <mishrabm2004@gmail.com>
2024-02-19 15:21:07 +0530

    While True

---
+++ ControlFlow/WhileLoop.cpp
@@ -0,0 +1,21 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    int i = 0;
+    int n;
+    cin >> n;
+
+    while (i < n)
+    {
+        i += 1;
+        cout << i << endl;
+    }
+
+    return 0;
+}
